dd51a2a4831b522fb3abdc088c4c33d3b89066a5
Vignesh Sankaran <developer@sankaran.io>
2019-09-20 23:08:10 +1000

    Cleanup and set minimum targets for macOS and iOS

--- Package.swift
+++ Package.swift
@@ -1,12 +1,13 @@
 // swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "PTVSwift",
+    platforms: [
+        .macOS(.v10_15), .iOS(.v12),
+    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "PTVSwift",
             targets: ["PTVSwift"]),
@@ -15,11 +16,9 @@ let package = Package(
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0-rc.2")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "PTVSwift",
-            dependencies: []),
+            dependencies: ["Alamofire"]),
         .testTarget(
             name: "PTVSwiftTests",
             dependencies: ["PTVSwift"]),
