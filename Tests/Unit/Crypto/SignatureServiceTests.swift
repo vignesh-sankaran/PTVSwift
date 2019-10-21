1f6cd410970dc903b4389257503bc45c298f6f34
Vignesh Sankaran <developer@sankaran.io>
2019-10-22 09:11:08 +1100

    Pass developer ID and security key for tests through environment variables

--- Tests/Unit/Crypto/SignatureServiceTests.swift
+++ Tests/Unit/Crypto/SignatureServiceTests.swift
@@ -5,28 +5,32 @@
 //  Created by Home on 18/10/19.
 //
 
+import Foundation
 import XCTest
 @testable import PTVSwift
 
 final class SignatureServiceTests: XCTestCase {
     
-    
+    var urlComponents = URLComponents()
     
     override func setUp() {
+       guard let devId = ProcessInfo.processInfo.environment["DEV_ID"] else {
+            fatalError("DEV_ID is not set as an environment variable!")
+        }
         
-    }
-    
-    func testSignURL() {
-        Configuration.devId = ""
-        Configuration.securityKey = ""
+        guard let securityKey = ProcessInfo.processInfo.environment["SECURITY_KEY"] else {
+            fatalError("SECURITY_KEY is not set as environment variable!")
+        }
         
-        var urlComponents = URLComponents()
+        Configuration.devId = devId
+        Configuration.securityKey = securityKey
         
         urlComponents.scheme = PROTOCOL
         urlComponents.host = BASE_URL
         urlComponents.path = "/\(VERSION)/routes"
-        
-        // Test that the signature component is not nil
+    }
+    
+    func testSignURL() {
         let signedURLComponents = SigningService().signURL(urlComponents: urlComponents)
         
         XCTAssertNotNil(signedURLComponents, "Signed URL is nil!")
